b2a56f294a48e2fb2fdfbf2b70af8cd62dc83808
N1k0l0zI0b1dze <nio63106@gmail.com>
2025-12-24 19:45:12 +0400

    Initialize Next.js portfolio project
    Set up a new Next.js project with TypeScript, ESLint, Tailwind CSS, and initial pages for About, Contact, and Portfolio. Added configuration files, global styles, a Navbar component, and SVG assets. Includes initial README and .gitignore.

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+const page = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+function Navbar() {
+  return (
+    <div className="h-full flex items-center justify-between px-4">
+      <div className="bg-black">1</div>
+      <div className="bg-black">2</div>
+    </div>
+  )
+}
+
+export default Navbar
